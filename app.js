a3d73b87dc62a861a407dc17c6c62cf389941ca6
Mohammad Aslam <Taiyab.aslam728@gmail.com>
2023-01-18 14:12:33 -0600

    added DOM elements

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 /* Imports */
 
 /* Get DOM Elements */
+const goblinsBeatenEl = document.getElementById('goblins-beaten');
+const goblinInputEl = document.getElementById('goblin-input');
+const summonButtonEl = document.getElementById('summon-button');
+const playerHpEl = document.getElementById('player-hp');
 
 /* State */
+let goblinsBeaten = 0;
+
+let playerHp = 5; 
 
 /* Events */
 
